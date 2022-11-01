670b88be4fa79421dd27390673a4dfad977776a0
DESKTOP-GC4IVE1\Ritz <okooko24816@gmail.com>
2022-11-01 22:51:59 +0900

    algorithm and math コメント追加

--- algorithms_and_math/009/src/main.rs
+++ algorithms_and_math/009/src/main.rs
@@ -11,13 +11,16 @@ fn main(){
     // v in arr は 配列内の要素を参照
     for element in arr
     {
-        println!("element={}",element);
+        // println!("element={}",element);
         // rev()は配列の反対から
         for i in (0..s).rev()
         {
             if dp[i] && i + element <= s
             {
-                println!("i+element={}+{}",i,element);
+                // println!("i+element={}+{}",i,element);
+                // 加算結果を保存
+                // element = 2 かつ i = 0 のとき 2+0 を保存
+                // element = 5 かつ i = 0,2 のとき 5+0,5+2 を保存(以下繰り返し)
                 dp[i + element] = true;
             }
         }
