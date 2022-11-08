81b16b9d385eb471508c363881bcbf17a4849ca8
DESKTOP-GC4IVE1\Ritz <okooko24816@gmail.com>
2022-11-08 23:55:36 +0900

    fix: 276 D

--- 276/D/src/main.rs
+++ 276/D/src/main.rs
@@ -30,15 +30,12 @@ fn main(){
 }
 
 fn get_common_divider(mut a:usize,mut b:usize) -> usize{
-    let mut zero = false;
-    while !zero{
+    // println!("{},{}",a,b);
+    while a != 0 && b != 0{
         if a <= b{
             b %= a;
         }else{
             a %= b;
-        }
-        if a == 0 || b == 0{
-            zero = true;
         }
     }
     if a <= b{
