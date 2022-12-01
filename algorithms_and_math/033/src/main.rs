5388a088ae687848a979d8d4b0c42732e7f17643
DESKTOP-GC4IVE1\Ritz <okooko24816@gmail.com>
2022-12-02 00:18:12 +0900

    algorithms and math 033

--- algorithms_and_math/033/src/main.rs
+++ algorithms_and_math/033/src/main.rs
@@ -9,19 +9,21 @@ fn main(){
         c1: f64,
         c2: f64,
     };
-    let bcx = b1-c1;
-    let bcy = b2-c2;
+    let bcx = c1-b1;
+    let bcy = c2-b2;
     let r2 = (bcx*bcx) + (bcy*bcy);
-    let tt = bcy*(b1-c1)+bcx*(b2-c2);
+    let tt = (bcx*(b1-a1)+bcy*(b2-a2)) * -1.0;
     let mut distance = 0.0;
     if tt < 0.0{
-        distance = (c1-a1)*(c1-a1)+(c2-a2)*(c1-a2);
+        distance = (b1-a1)*(b1-a1)+(b2-a2)*(b2-a2);
     }else if tt > r2{
-        distance = (b1-a1)*(b1-a1)+(b2-a2)*(b1-a2);
+        distance = (c1-a1)*(c1-a1)+(c2-a2)*(c2-a2);
     }else{
-        distance = (bcx * (c1-a1) - bcy*(c2-a2))/r2;
+        let f = (bcx*(b2-a2))-(bcy*(b1-a1));
+        distance = (f*f)/r2;
     }
-
+    // println!("r2={},tt={},distance={}",r2,tt,distance);
+    // println!("{}",((b1-a1)*(b1-a1)+(b2-a2)*(b2-a2)).sqrt());
+    // println!("{}",((c1-a1)*(c1-a1)+(c2-a2)*(c2-a2)).sqrt());
     println!("{}",distance.sqrt());
-    
 }
