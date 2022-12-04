d825a807f07dc3cdb8884baa9e15f445bb91477b
DESKTOP-GC4IVE1\Ritz <okooko24816@gmail.com>
2022-12-04 20:01:12 +0900

    abc 280 D

--- 280/D/src/main.rs
+++ 280/D/src/main.rs
@@ -8,30 +8,39 @@ fn main(){
     let mut m = n;
     let mut count = 2;
     let mut count_arr:HashMap<usize, usize> = HashMap::new();
-
-    let mut arr = vec![];
-    while m > 1{
+    while count * count <= n{
         if m % count == 0{
             if !count_arr.contains_key(&count){
-                arr.insert(count,1);
+                count_arr.insert(count,1);
+            }else{
+                *count_arr.entry(count).or_insert(0) += 1;
             }
-            *count_arr.entry(count).or_insert(0) += 1;
-            // count_arr.insert(count,insert);
             m /= count;
         }else{
             count += 1;
         }
     }
-    // let mut arr2 = vec![];
-    // for i in 0..1000000000000{
-    //     if count_arr[i] > 0{
-    //         let mut num = i;
-    //         for _j in 0..count_arr[i]{
-    //             num *= i;
-    //         }
-    //         arr2.push(num);
-    //     }
-    // }
-    // arr2.sort_unstable();
-    // println!("{}",arr[arr2.len()-1]);
+    if m > 1{
+        *count_arr.entry(m).or_insert(0) += 1; 
+    }
+    let mut arr = vec![];
+    for (i,j) in &count_arr{
+        let mut num = *i;
+        let mut element_count = 1;
+        while element_count < *j{
+            num += *i;
+            let mut num_cp = num;
+            while num_cp > 1{
+                if num_cp % *i == 0{
+                    element_count+=1;
+                    num_cp /= *i;
+                }else{
+                    break;
+                }
+            }
+        }
+        arr.push(num);
+    }
+    arr.sort_unstable();
+    println!("{}",arr[arr.len()-1]);
 }
