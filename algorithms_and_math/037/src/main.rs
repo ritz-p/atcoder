3cffd0cba2ffe19cf63d471e6742e4369ff45374
DESKTOP-GC4IVE1\Ritz <okooko24816@gmail.com>
2022-12-09 02:29:14 +0900

    wip

--- algorithms_and_math/037/src/main.rs
+++ algorithms_and_math/037/src/main.rs
@@ -4,6 +4,30 @@ fn main(){
     input!{
         points: [(f64,f64);4],
     };
+    if (y3 - y1) * (x1 - x2) == (y1 - y2) * (x3 - x1)
+    && (y4 - y1) * (x1 - x2) == (y1 - y2) * (x4 - x1)
+{
+    let mut a = (x1, y1);
+    let mut b = (x2, y2);
+    let mut c = (x3, y3);
+    let mut d = (x4, y4);
+
+    if a > b {
+        mem::swap(&mut a, &mut b);
+    }
+
+    if c > d {
+        mem::swap(&mut c, &mut d);
+    }
+
+    if cmp::max(a, c) <= cmp::min(b, d) {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
+
+    return;
+}
     let s1 = (points[0].0 - points[1].0) * (points[2].1 - points[0].1) + (points[0].1 - points[1].1) * (points[0].0 - points[2].0);
     let s2 = (points[0].0 - points[1].0) * (points[3].1 - points[0].1) + (points[0].1 - points[1].1) * (points[0].0 - points[3].0);
     let t1 = (points[2].0 - points[3].0) * (points[0].1 - points[2].1) + (points[2].1 - points[3].1) * (points[2].0 - points[0].0);
