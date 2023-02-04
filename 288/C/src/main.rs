a3923c01ca586a4c570d4c3b8ee1df941b0aa88e
DESKTOP-GC4IVE1\Ritz <okooko24816@gmail.com>
2023-02-04 22:44:24 +0900

    ABC288 C

--- 288/C/src/main.rs
+++ 288/C/src/main.rs
@@ -1,40 +1,66 @@
-use proconio::{input, marker::Chars};
- 
- 
+use proconio::input;
+
+fn dfs(map: &Vec<Vec<usize>>, mut visited: Vec<bool>, mut searched: Vec<bool>, mut ans: usize, key: usize) -> (usize, Vec<bool>, Vec<bool>) {
+    if visited[key] {
+        return (ans, visited, searched);
+    }
+
+    visited[key] = true;
+    searched[key] = true;
+
+    for &k in map[key].iter() {
+
+        if searched[k] {
+            continue;
+        }
+
+        if visited[k] {
+            ans += 1;
+            continue;
+        }
+
+        let (a, v, s) = dfs(map, visited, searched, ans, k);
+        ans = a;
+        visited = v;
+        searched = s;
+    }
+
+    searched[key] = false;
+    (ans, visited, searched)
+}
+
 fn main() {
-    input! {
+    input!{
         n: usize,
         m: usize,
-        uv: [(usize, usize); m],
     };
- 
-    let mut graph = vec![vec![]; n];
- 
-    for (u, v) in uv {
-        graph[u - 1].push(v - 1);
-        graph[v - 1].push(u - 1);
-    }
- 
+
     let mut visited = vec![false; n];
+    let mut searched = vec![false; n];
+    let mut map: Vec<Vec<usize>> = vec![vec![]; n];
+
+    for _ in 0..m {
+        input!{
+            a: usize,
+            b: usize,
+        };
+
+        let a = a-1;
+        let b = b-1;
+
+        map[a].push(b);
+        map[b].push(a);
+    }
+
     let mut ans = 0;
- 
-    for v in 0..n {
-        if !visited[v] {
-            // 最初に1と繋がるところを訪れたことにする
-            dfs(v, &graph, &mut visited);
-            ans += 1;
+    for i in 0..n {
+        if !visited[i] {
+            let (a, v, s) = dfs(&map, visited, searched, ans, i);
+            ans = a;
+            visited = v;
+            searched = s;
         }
     }
- 
+
     println!("{}", ans);
-}
- 
-fn dfs(v: usize, graph: &Vec<Vec<usize>>, visited: &mut Vec<bool>) {
-    visited[v] = true;
- 
-    for &w in graph[v].iter() {
-        if !visited[w] {
-            dfs(w, graph, visited);
-        }
-    }
 }
