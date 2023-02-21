ad500ed7db36ef06a5a70f138edb7f824d4cf20a
DESKTOP-GC4IVE1\Ritz <okooko24816@gmail.com>
2023-02-21 20:01:47 +0900

    add comment

--- 290/D/src/main.rs
+++ 290/D/src/main.rs
@@ -1,6 +1,8 @@
 use proconio::input;
 use num_integer::gcd;
-
+/*
+A,B を正整数とし、A と B の最大公約数を g とおく。A=ag,B=bg と表されるとき、a 個の数 0,BmodA,2BmodA,…,(a−1)BmodA の中には、0,g,2g,…,(a−1)g (すなわち 0 以上 A 未満の g の倍数) がちょうど 1 回ずつ現れる。
+*/
 fn main(){
     input!{
         t: usize,
