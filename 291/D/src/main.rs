9c8b95caed60ae215931573b8cb2fdaf57bcb914
DESKTOP-GC4IVE1\Ritz <okooko24816@gmail.com>
2023-02-28 22:28:22 +0900

    fix: ABC 291 D

--- 291/D/src/main.rs
+++ 291/D/src/main.rs
@@ -3,23 +3,29 @@ use proconio::input;
 fn main() {
     input!{
         n: usize,
-        tup: [[usize;2];n],
+        tup: [(usize,usize);n],
     };
-    let m = 998244353;
-    let mut dp = vec![vec![0,0];n+1];
-    dp[0] = vec![1,1];
-    for i in 1..n{
-        for pre in 0..2{
-            for next in 0..2{
-                if tup[i-1][pre] != tup[i][next] {
-                    dp[i][next] += dp[i-1][pre];
-                }
-            }
+    let div = 998244353;
+    let mut dp = vec![(1,1)];
+    let win = tup.windows(2);
+    for (i,val) in win.enumerate(){
+        let mut x=0;
+        let mut y=0;
+        if val[1].0 != val[0].0{
+            x = dp[i].0
         }
-        dp[i][0] %= m;
-        dp[i][1] %= m;
+        if val[1].0 != val[0].1{
+            x += dp[i].1
+        }
+        x %= div;
+        if val[1].1 != val[0].0{
+            y = dp[i].0
+        }
+        if val[1].1 != val[0].1{
+            y += dp[i].1
+        }
+        y %= div;
+        dp.push((x,y));
     }
-
-
-    println!("{}",(dp[n-1][0]+dp[n-1][1])%m);
+    println!("{}",(dp[n-1].0 + dp[n-1].1) % div);
 }
