54305a6c1d96a1c203f3ee12278a10723aaa6e1d
DESKTOP-GC4IVE1\Ritz <okooko24816@gmail.com>
2023-03-20 00:18:43 +0900

    add: auto create atcoder set srcript

---
+++ prepare.sh
@@ -0,0 +1,25 @@
+mkdir $1
+
+alphabet=(A B C D E F)
+for abc in ${alphabet[@]}
+do
+    mkdir -p $1/$abc/src
+    touch $1/$abc/Cargo.toml
+    touch $1/$abc/src/main.rs
+    echo "[package]
+name = "$1_${abc,}"
+version = "0.1.0"
+authors = ["root"]
+edition = "2018"
+
+[dependencies]
+proconio = "0.4.3"
+itertools = "0.10.5"" > $1/$abc/Cargo.toml
+    echo "use proconio::input;
+    
+fn main(){
+    input!{
+
+    };
+}" > $1/$abc/src/main.rs
+done
