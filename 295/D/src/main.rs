8efcf71229dd9b9ec4760c892e2fcb7c34f5fced
DESKTOP-GC4IVE1\Ritz <okooko24816@gmail.com>
2023-03-26 18:24:50 +0900

    ABC 295 D

--- 295/D/src/main.rs
+++ 295/D/src/main.rs
@@ -2,6 +2,16 @@ use proconio::input;
     
 fn main(){
     input!{
-
+        s: String,
     };
+    let mut flag = 0;
+    let mut bk = vec![0_u64;1 << 10];
+    bk[0] = 1;
+    let mut res = 0;
+    for c in s.chars(){
+        flag ^= 1 << c as usize - '0' as usize;
+        res += bk[flag as usize];
+        bk[flag as usize] += 1;
+    }
+    println!("{}",res);
 }
