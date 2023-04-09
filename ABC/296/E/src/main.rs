557595b468b7086173bc3d2baa81b1c79d6020bd
DESKTOP-GC4IVE1\Ritz <okooko24816@gmail.com>
2023-04-09 16:58:51 +0900

    ABC E

--- ABC/296/E/src/main.rs
+++ ABC/296/E/src/main.rs
@@ -1,7 +1,23 @@
 use proconio::input;
-    
-fn main(){
-    input!{
+use proconio::marker::*;
 
+fn main() {
+    input! {
+        n: usize,
+        mut a: [Usize1; n]
     };
+    let c: usize = 2usize*10usize.pow(5)+1;
+    for _ in 0..30 {
+        a = a.iter().map(|&x| a[x]).collect::<Vec<usize>>();
+    }
+    let mut visited = vec![false; c];
+    let mut answer = 0usize;
+    for &a in a.iter() {
+        if visited[a] {
+            continue;
+        }
+        visited[a] = true;
+        answer += 1;
+    }
+    println!("{}", answer)
 }
