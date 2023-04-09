7785387387d6f95ef28613b2f50973c3cdda4b62
DESKTOP-GC4IVE1\Ritz <okooko24816@gmail.com>
2023-04-09 16:35:49 +0900

    fix: move to ABC directory

--- prepare.sh
+++ prepare.sh
@@ -3,9 +3,9 @@ mkdir $1
 alphabet=(A B C D E F G Ex)
 for abc in ${alphabet[@]}
 do
-    mkdir -p $1/$abc/src
-    touch $1/$abc/Cargo.toml
-    touch $1/$abc/src/main.rs
+    mkdir -p ABC/$1/$abc/src
+    touch ABC/$1/$abc/Cargo.toml
+    touch ABC/$1/$abc/src/main.rs
     echo "[package]
 name = \"$1_${abc,}\"
 version = \"0.1.0\"
@@ -14,12 +14,12 @@ edition = \"2018\"
 
 [dependencies]
 proconio = \"0.4.3\"
-itertools = \"0.10.5\"" > $1/$abc/Cargo.toml
+itertools = \"0.10.5\"" > ABC/$1/$abc/Cargo.toml
     echo "use proconio::input;
     
 fn main(){
     input!{
 
     };
-}" > $1/$abc/src/main.rs
+}" > ABC/$1/$abc/src/main.rs
 done
