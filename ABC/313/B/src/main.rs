b6f53325bed817d0127f3d8375e26aab123fc144
DESKTOP-GC4IVE1\Ritz <okooko24816@gmail.com>
2023-08-05 23:34:07 +0900

    ABC 313 B

--- ABC/313/B/src/main.rs
+++ ABC/313/B/src/main.rs
@@ -1,42 +1,13 @@
 use proconio::input;
-
-fn main(){
-    input!{
-        n: usize,
-        m: usize,
-        tup: [(usize,usize);m]
-    };
-    let mut a = vec![vec![];n+1];
-    let mut b = vec![vec![];n+1];
-
-    for i in 0..m{
-        a[tup[i].0].push(tup[i].1);
-        b[tup[i].1].push(tup[i].0);
-    }
-    let a_cl = a.clone();
-
-    for i in 1..n{
-        for j in &a_cl[i]{
-            dfs(&mut a,&a_cl,i,*j);
+use itertools::Itertools;
+fn main() {
+    input! {n: usize, ab: [(usize, usize)]}
+    let mut ans = vec![];
+    for i in 0..n {
+        let n = ab.iter().filter(|(_, node)| *node == i+1).collect_vec();
+        if n.len() == 0 {
+            ans.push(i + 1);
         }
     }
-    for i in 1..n{
-        if a[i].len() == n-1{
-            println!("{}",i+1);
-            return;
-        }
-    }
-    println!("{:?}",a);
-    println!("-1");
-}
-
-fn dfs(graph: &mut Vec<Vec<usize>>,graph_cl: &Vec<Vec<usize>>,base: usize,index: usize){
-    if graph[index].len() == 0{
-        return
-    }else{
-        for g in 0..graph[index].len(){
-            graph[base].push(graph_cl[index][g]);
-            dfs(graph,graph_cl,base,graph[index][g]);
-        }
-    }
+    println!("{}", if ans.len() == 1 {ans[0].to_string()} else {"-1".to_string()});
 }
