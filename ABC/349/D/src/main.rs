4c5eeba64c761eb3b01fad14fc96c65b903538b2
DESKTOP-GC4IVE1\Ritz <okooko24816@gmail.com>
2024-04-15 23:46:31 +0900

    ABC 349 D

--- ABC/349/D/src/main.rs
+++ ABC/349/D/src/main.rs
@@ -2,6 +2,25 @@ use proconio::input;
 
 fn main(){
     input!{
-
+        mut l: usize,
+        r: usize,
     };
+
+    let mut res:Vec<(usize,usize)> = vec![];
+
+    loop{
+        if l == r{
+            break;
+        }
+        let mut coe: u32 = 0;
+        while l % 2usize.pow(coe+1) == 0 && l + 2usize.pow(coe+1) <= r{
+            coe += 1;
+        }
+        res.push((l,l+2usize.pow(coe)));
+        l += 2usize.pow(coe);
+    }
+    println!("{}",res.len());
+    for (x,y) in res{
+        println!("{} {}",x,y);
+    }
 }
