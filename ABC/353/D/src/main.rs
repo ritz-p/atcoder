ef940fcd3bc9bad37855743b48713ae136caaf0e
DESKTOP-GC4IVE1\Ritz <okooko24816@gmail.com>
2024-05-14 21:06:50 +0900

    ABC 353 D

--- ABC/353/D/src/main.rs
+++ ABC/353/D/src/main.rs
@@ -1,15 +1,34 @@
 use proconio::input;
 
-fn main(){
-    input!{
+fn main() {
+    input! {
         n: usize,
-        ss: [usize;n]
-    };
-    let mut res = 0;
-    let mut m = 0;
-    let mut s = 0;
-    for i in ss.iter().rev(){
-        res += i * m + s;
-        let l = i.to_string().len();
+        a: [usize; n]
     }
+    let m = 998244353;
+
+    let mut digits:Vec<usize> = vec![0;11];
+    for i in 0..n{
+        digits[a[i].to_string().len()] += 1;
+    }
+    let mut res = 0;
+    for i in 0..n{
+        for (index,k) in digits.iter().enumerate(){
+            if *k == 0{
+                continue;
+            }
+            let mut digit = 1;
+            for _ in 0..index{
+                digit *= 10;
+            }
+            let num = a[i] * digit % m;
+            res += num * k % m;
+            if index == a[i].to_string().len(){
+                res -= (digit * a[i]) % m;
+            }
+        }
+        digits[a[i].to_string().len()] -= 1;
+        res += (a[i] * i) % m;
+    }
+    println!("{}", res % m);
 }
