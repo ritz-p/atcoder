67b06de9b2b09db8978bb03a155bbcf4d34a4cc5
DESKTOP-GC4IVE1\Ritz <okooko24816@gmail.com>
2024-05-19 01:27:19 +0900

    ABC 354 D

--- ABC/354/D/src/main.rs
+++ ABC/354/D/src/main.rs
@@ -2,6 +2,27 @@ use proconio::input;
 
 fn main(){
     input!{
-
+        a: isize,
+        b: isize,
+        c: isize,
+        d: isize,
     };
+    let m = 1000000000;
+    let a = (a + m) as usize;
+    let b = (b + m) as usize;
+    let c = (c + m) as usize;
+    let d = (d + m) as usize;
+
+
+    println!("{}",f(c,d) + f(a,b) - f(c,b) - f(a,d));
+}
+
+fn f(x: usize,y: usize) -> usize{
+    let arr = vec![vec![0,0,0,0,0],vec![0,2,3,3,4],vec![0,3,6,7,8]];
+    let s1 = (y / 2) * (x / 4) * arr[2][4];
+    let s2 = (y / 2) * arr[2][x % 4];
+    let s3 = (x / 4) * arr[y % 2][4];
+    let s4 = arr[y % 2][x % 4];
+
+    s1 + s2 + s3 + s4
 }
