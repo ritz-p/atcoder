5ec569a0e55e537255d1f2904dc4ea0948f5cef5
DESKTOP-GC4IVE1\Ritz <okooko24816@gmail.com>
2024-05-30 23:10:50 +0900

    ABC 252 C~D

--- ABC/252/src/c.rs
+++ ABC/252/src/c.rs
@@ -1,20 +1,23 @@
-use proconio::{input, marker::Chars};
+use proconio::input;
+use std::collections::BTreeMap;
 fn main() {
     input! {
         n: usize,
-        s: [Chars; n]
+        s: [String; n]
     }
-    let mut ans = std::usize::MAX;
-    for i in 0..10 {
-        let mut cnt = vec![0; 10];
-        for j in 0..n {
-            let index = s[j].iter().position(|&si| si as u8 - b'0' == i).unwrap();
-            cnt[index] += 1;
+    let mut res = std::usize::MAX;
+    for y in "0123456789".chars() {
+        let mut bmap = BTreeMap::new();
+        for x in s.iter() {
+            let f = x.find(y).unwrap();
+            *bmap.entry(f).or_insert(0) += 1;
         }
-        let cnt_max = *cnt.iter().max().unwrap();
-        let r = cnt.iter().rposition(|&c| c == cnt_max).unwrap();
-        let tmp = r + (cnt_max - 1) * 10;
-        ans = ans.min(tmp);
+        let mut mx = 0;
+        for (&v, &x) in bmap.iter() {
+            mx = mx.max(v + 10 * (x - 1));
+        }
+        res = res.min(mx);
     }
-    println!("{ans}");
+
+    println!("{}",res);
 }
--- ABC/252/src/d.rs
+++ ABC/252/src/d.rs
@@ -1,7 +1,18 @@
+use itertools::Itertools;
 use proconio::input;
-
 fn main(){
     input!{
-
+        n: usize,
+        a: [usize;n]
     };
+    let mut res = n * (n - 1) * (n - 2) / 6;
+
+    for (_index,num) in a.iter().counts(){
+        if num >= 2{
+            res -= num * (num - 1) * (num - 2) / 6;
+            res -= (n - num) * num * (num - 1) / 2;
+        }
+    }
+
+    println!("{}",res);
 }
