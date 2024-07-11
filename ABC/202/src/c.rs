557eb194fce14729ce461757e0a1f08ead5695eb
DESKTOP-GC4IVE1\Ritz <okooko24816@gmail.com>
2024-07-11 22:25:54 +0900

    ABC 202 C

---
+++ ABC/202/src/c.rs
@@ -0,0 +1,26 @@
+use proconio::input;
+use std::collections::HashMap;
+fn main(){
+    input!{
+        n: usize,
+        a: [usize;n],
+        b: [usize;n],
+        c: [usize;n]
+    };
+
+    let mut map = HashMap::new();
+
+    for i in c{
+        *map.entry(b[i-1]).or_insert(0) += 1;
+    }
+
+    let mut res:usize = 0;
+
+    for e in &a{
+        if let Some(v) = map.get(e){
+            res += v;
+        }
+    }
+
+    println!("{}",res);
+}
