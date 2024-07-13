8df2efd6359d345c995ba9d9731cc5da22d34e1a
DESKTOP-GC4IVE1\Ritz <okooko24816@gmail.com>
2024-07-14 00:01:10 +0900

    ABC 209 C

---
+++ ABC/209/src/c.rs
@@ -0,0 +1,15 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        n: usize,
+        mut c: [usize;n]
+    };
+    let mut res = 1;
+    c.sort();
+    for i in 0..n{
+        res = res * (c[i] - i).max(0) % 1000000007;
+    }
+
+    println!("{}",res);
+}
