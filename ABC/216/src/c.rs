e0c72c52e77b529845058ae5dd71df9bca3d372c
DESKTOP-GC4IVE1\Ritz <okooko24816@gmail.com>
2024-07-18 00:25:02 +0900

    ABC 215 C

---
+++ ABC/216/src/c.rs
@@ -0,0 +1,22 @@
+use itertools::Itertools;
+use proconio::input;
+
+fn main() {
+    input! {
+        mut n: usize,
+    };
+
+    let mut res = vec![];
+
+    while n != 0 {
+        if n % 2 == 0 {
+            n /= 2;
+            res.push('B');
+        } else {
+            n -= 1;
+            res.push('A');
+        }
+    }
+
+    println!("{}", res.iter().rev().join(""));
+}
