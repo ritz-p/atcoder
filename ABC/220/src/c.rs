72e2489027ec7a899e8c41be7533096ab5057bf1
DESKTOP-GC4IVE1\Ritz <okooko24816@gmail.com>
2024-07-31 23:33:11 +0900

    ABC 220 C

---
+++ ABC/220/src/c.rs
@@ -0,0 +1,20 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        n: usize,
+        a: [usize;n],
+        mut x: usize,
+    };
+    let sum = a.iter().sum::<usize>();
+    let div = x / sum;
+    x %= sum;
+    for (index,e) in a.iter().enumerate(){
+        if x >= *e{
+            x -= e;
+        }else{
+        println!("{}",div * n + index + 1);
+            return;
+        }
+    }
+}
