e132bdaf4c01d7e565fb57c4120ff80890525182
DESKTOP-GC4IVE1\Ritz <okooko24816@gmail.com>
2024-08-25 03:12:44 +0900

    ABC 368 D

--- ABC/368/src/d.rs
+++ ABC/368/src/d.rs
@@ -1,68 +1,41 @@
-use proconio::input;
+use std::collections::HashSet;
+
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
         n: usize,
         k: usize,
         ab: [(usize,usize);n-1],
-        v: [usize;k]
+        v: [Usize1;k]
     };
-
-    let mut uf = UnionFind::new(n + 1);
-
-    let mut required_set = vec![false; n + 1];
-    for &vi in &v {
-        required_set[vi] = true;
-    }
-
+    let mut graph = vec![vec![]; n];
     for (a, b) in ab {
-        uf.union(a, b);
+        graph[a - 1].push(b - 1);
+        graph[b - 1].push(a - 1);
     }
-    let root = uf.find(v[0]);
-    let result = uf.size(root);
-    println!("{}", result);
+    let start = v[0];
+    let v = v.into_iter().collect::<HashSet<usize>>();
+    let mut visited = vec![false; n];
+    let res = dfs(&graph, &v, &mut visited, start);
+    println!("{}", res);
 }
 
-struct UnionFind {
-    parent: Vec<usize>,
-    size: Vec<usize>,
-}
+fn dfs(g: &[Vec<usize>], v: &HashSet<usize>, visited: &mut [bool], node: usize) -> i64 {
+    let mut cnt = 0;
 
-impl UnionFind {
-    fn new(n: usize) -> Self {
-        UnionFind {
-            parent: (0..=n).collect(),
-            size: vec![1; n + 1],
+    visited[node] = true;
+
+    for &nnode in &g[node] {
+        if visited[nnode] {
+            continue;
         }
+        cnt += dfs(g, v, visited, nnode);
     }
 
-    fn find(&mut self, x: usize) -> usize {
-        if self.parent[x] != x {
-            self.parent[x] = self.find(self.parent[x]);
-        }
-        self.parent[x]
+    if v.contains(&node) || cnt != 0 {
+        cnt += 1;
     }
 
-    fn union(&mut self, x: usize, y: usize) -> bool {
-        let root_x = self.find(x);
-        let root_y = self.find(y);
-
-        if root_x != root_y {
-            if self.size[root_x] < self.size[root_y] {
-                self.parent[root_x] = root_y;
-                self.size[root_y] += self.size[root_x];
-            } else {
-                self.parent[root_y] = root_x;
-                self.size[root_x] += self.size[root_y];
-            }
-            true
-        } else {
-            false
-        }
-    }
-
-    fn size(&mut self, x: usize) -> usize {
-        let root_x = self.find(x);
-        self.size[root_x]
-    }
+    cnt
 }
