4d0200b20e3f5fc1330bebed52713c03da27c34b
DESKTOP-GC4IVE1\Ritz <okooko24816@gmail.com>
2024-09-07 23:34:52 +0900

    ABC 370 D

--- ABC/370/src/d.rs
+++ ABC/370/src/d.rs
@@ -7,35 +7,36 @@ fn main() {
         q: usize,
         rc: [(usize,usize);q]
     };
-    let mut row_walls: Vec<BTreeSet<usize>> = vec![(1..=w).collect(); h + 1];
-    let mut col_walls: Vec<BTreeSet<usize>> = vec![(1..=h).collect(); w + 1];
+    let mut row: Vec<BTreeSet<usize>> = vec![(1..=w).collect(); h + 1];
+    let mut col: Vec<BTreeSet<usize>> = vec![(1..=h).collect(); w + 1];
     let mut walls = HashSet::new();
     for (r, c) in rc {
         if walls.contains(&(r, c)) {
-            if let Some(&nearest_row) = row_walls[r].range(c..).next() {
-                walls.insert((r, nearest_row));
-                row_walls[r].remove(&nearest_row);
+            if let Some(&right) = row[r].range(c + 1..).next() {
+                walls.insert((r, right));
+                row[r].remove(&right);
+                col[right].remove(&r);
             }
-            if let Some(&nearest_row_up) = row_walls[r].range(..c).next_back() {
-                walls.insert((r, nearest_row_up));
-                row_walls[r].remove(&nearest_row_up);
+            if let Some(&left) = row[r].range(..c).next_back() {
+                walls.insert((r, left));
+                row[r].remove(&left);
+                col[left].remove(&r);
             }
-            if let Some(&nearest_col) = col_walls[c].range(r..).next() {
-                walls.insert((nearest_col, c));
-                col_walls[c].remove(&nearest_col);
+            if let Some(&down) = col[c].range(r + 1..).next() {
+                walls.insert((down, c));
+                row[down].remove(&c);
+                col[c].remove(&down);
             }
-            if let Some(&nearest_col_up) = col_walls[c].range(..r).next_back() {
-                walls.insert((nearest_col_up, c));
-                col_walls[c].remove(&nearest_col_up);
+            if let Some(&up) = col[c].range(..r).next_back() {
+                walls.insert((up, c));
+                row[up].remove(&c);
+                col[c].remove(&up);
             }
         } else {
             walls.insert((r, c));
-            row_walls[r].remove(&c);
-            col_walls[c].remove(&r);
+            row[r].remove(&c);
+            col[c].remove(&r);
         }
     }
-    println!("{:?}", row_walls);
-    println!("{:?}", col_walls);
-    println!("{:?}", walls);
     println!("{}", h * w - walls.len());
 }
