ef0c5429b1733c6d780b19111a7adf1d8af9b4b2
ritz-p <okooko24816@gmail.com>
2024-10-03 01:51:57 +0900

    ABC 220 D

--- ABC/220/src/d.rs
+++ ABC/220/src/d.rs
@@ -1,5 +1,21 @@
 use proconio::input;
 
 fn main() {
-    input! {};
+    input! {
+        n: usize,
+        a: [usize;n]
+    };
+    let m:usize = 998244353;
+    let mut dp = vec![vec![0; 10]; n];
+    dp[0][a[0]] = 1;
+    for i in 1..n {
+        for j in 0..=9 {
+            dp[i-1][j] %= m;
+            dp[i][(j+a[i])%10] += dp[i-1][j];
+            dp[i][(j*a[i])%10] += dp[i-1][j];
+        }
+    }
+    for j in 0..=9 {
+        println!("{}", dp[n-1][j] % m);
+    }
 }
