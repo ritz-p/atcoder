19057b6a4739d2cc1046a2a23c4283329f381aeb
ritz-p <ritz-p@users.noreply.github.com>
2024-10-07 17:01:59 +0000

    fix: format rust file

--- ABC/233/src/c.rs
+++ ABC/233/src/c.rs
@@ -3,32 +3,32 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        x: usize,  
+        x: usize,
     };
     let mut a = vec![];
-    for _i in 0..n{
-        input!{
+    for _i in 0..n {
+        input! {
             l: usize,
             la: [usize;l]
         };
         a.push(la);
     }
     let mut res = 0;
-    dfs(&a,0,1,n,x,&mut res);
-    println!("{}",res);
+    dfs(&a, 0, 1, n, x, &mut res);
+    println!("{}", res);
 }
 
-fn dfs(a: &Vec<Vec<usize>>,current: usize,total: usize,n: usize,x: usize,res: &mut usize){
-    if current == n{
-        if total == x{
+fn dfs(a: &Vec<Vec<usize>>, current: usize, total: usize, n: usize, x: usize, res: &mut usize) {
+    if current == n {
+        if total == x {
             *res += 1;
         }
         return;
     }
-    for c in &a[current]{
-        if total > x / c{
+    for c in &a[current] {
+        if total > x / c {
             continue;
         }
-        dfs(a,current+1,total*c,n,x,res);
+        dfs(a, current + 1, total * c, n, x, res);
     }
 }
--- ABC/233/src/d.rs
+++ ABC/233/src/d.rs
@@ -8,18 +8,18 @@ fn main() {
         k: isize,
         a: [isize;n]
     };
-    let mut sums = vec![0;n+1];
-    for i in 0..n{
-        sums[i+1] = sums[i] + a[i];
+    let mut sums = vec![0; n + 1];
+    for i in 0..n {
+        sums[i + 1] = sums[i] + a[i];
     }
 
-    let mut map: HashMap<isize,usize> = HashMap::new();
+    let mut map: HashMap<isize, usize> = HashMap::new();
     let mut res = 0;
-    for i in 1..=n{
-        *map.entry(sums[i-1]).or_insert(0) += 1;
-        if let Some(v) = map.get(&(sums[i] - k)){
+    for i in 1..=n {
+        *map.entry(sums[i - 1]).or_insert(0) += 1;
+        if let Some(v) = map.get(&(sums[i] - k)) {
             res += v;
         }
     }
-    println!("{}",res);
+    println!("{}", res);
 }
