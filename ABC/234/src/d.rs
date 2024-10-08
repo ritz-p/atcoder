53a8e3e3871bd5abdfda0c1d3ee81d2fdb36a989
ritz-p <okooko24816@gmail.com>
2024-10-09 01:27:02 +0900

    ABC 234 D

--- ABC/234/src/d.rs
+++ ABC/234/src/d.rs
@@ -1,5 +1,24 @@
 use proconio::input;
-
 fn main() {
-    input! {};
+    input! {
+        n: usize,
+        k: usize,
+        p: [usize;n]
+    };
+    let mut v = vec![false; n + 1];
+    for i in 0..k {
+        v[p[i]] = true;
+    }
+    let mut x = p[..k].iter().copied().min().unwrap();
+    println!("{}", x);
+    for i in k..n {
+        if p[i] > x {
+            v[x] = false;
+            v[p[i]] = true;
+        }
+        while !v[x] {
+            x += 1;
+        }
+        println!("{}", x);
+    }
 }
