50275c7bea326e4d593bffc1990eb2846ff5cf1d
ritz-p <okooko24816@gmail.com>
2024-10-31 20:00:00 +0900

    ABC 236 C

---
+++ ABC/236/src/c.rs
@@ -0,0 +1,24 @@
+use std::collections::HashSet;
+
+use proconio::input;
+
+fn main() {
+    input! {
+        n: usize,
+        m: usize,
+        s: [String;n],
+        t: [String;m]
+    };
+
+    let mut set = HashSet::new();
+    for st in t{
+        set.insert(st);
+    }
+    for ss in s{
+        if set.contains(&ss){
+            println!("Yes");
+        }else{
+            println!("No");
+        }
+    }
+}
