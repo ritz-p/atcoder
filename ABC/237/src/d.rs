f9b29df1073c1db8acdc06752dc75900e31dcb45
ritz-p <okooko24816@gmail.com>
2024-11-14 19:04:26 +0900

    ABC 237 D

--- ABC/237/src/d.rs
+++ ABC/237/src/d.rs
@@ -1,5 +1,25 @@
-use proconio::input;
-
+use itertools::Itertools;
+use proconio::{input, marker::Chars};
+use std::collections::VecDeque;
 fn main() {
-    input! {};
+    input! {
+        n: usize,
+        s: Chars
+    };
+    let mut res = VecDeque::new();
+    res.push_back(n);
+
+    for (index, c) in s.iter().rev().enumerate() {
+        match *c {
+            'R' => {
+                res.push_front(n - index - 1);
+            }
+            'L' => {
+                res.push_back(n - index - 1);
+            }
+            _ => unreachable!(),
+        }
+    }
+
+    println!("{}", res.iter().join(" "));
 }
