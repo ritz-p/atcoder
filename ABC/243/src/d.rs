1758bc28aeaa2e53f09f8569c5c0df95ea669094
ritz-p <okooko24816@gmail.com>
2024-12-03 21:28:03 +0900

    ABC 243 D

--- ABC/243/src/d.rs
+++ ABC/243/src/d.rs
@@ -1,5 +1,36 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
-    input! {};
+    input! {
+        n: usize,
+        mut x: usize,
+        s: Chars
+    };
+    let mut v = vec![];
+    for i in 0..n {
+        if let Some(c) = v.get(v.len() - 1) {
+            if s[i] == 'U' && matches!(c, 'L' | 'R') {
+                v.pop().unwrap();
+                continue;
+            }
+        }
+        v.push(s[i]);
+    }
+
+    for c in v {
+        match c {
+            'U' => {
+                x /= 2;
+            }
+            'R' => {
+                x *= 2;
+                x += 1;
+            }
+            'L' => {
+                x *= 2;
+            }
+            _ => {}
+        }
+    }
+    println!("{}", x);
 }
