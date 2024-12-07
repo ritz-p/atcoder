6832fa7b5a88a01223f8c343ce9dfea0918c1bd0
ritz-p <okooko24816@gmail.com>
2024-12-07 20:27:42 +0900

    ABC 244 D

--- ABC/244/src/d.rs
+++ ABC/244/src/d.rs
@@ -1,5 +1,11 @@
 use proconio::input;
 
 fn main() {
-    input! {};
+    input! {
+        s: [char;3],
+        t: [char;3],
+    };
+    let diff = (0..3).filter(|&i| s[i] != t[i]).count();
+
+    println!("{}", if diff == 2 { "No" } else { "Yes" });
 }
