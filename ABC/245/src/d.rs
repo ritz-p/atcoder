10d9def04177d780e91f6842ad0a428c0186f567
ritz-p <okooko24816@gmail.com>
2024-12-13 22:15:56 +0900

    ABC 245 D

--- ABC/245/src/d.rs
+++ ABC/245/src/d.rs
@@ -1,5 +1,20 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
-    input! {};
+    input! {
+        n: usize,
+        m: usize,
+        a: [isize;n+1],
+        mut c: [isize;n + m+1]
+    };
+    let mut b = vec![0; m + 1];
+    for i in (0..=m).rev() {
+        b[i] = c[i + n] / a[n];
+        for j in (0..=n).rev() {
+            c[i + j] -= b[i] * a[j];
+        }
+    }
+
+    println!("{}", b.iter().join(" "));
 }
