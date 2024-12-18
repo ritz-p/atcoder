9db5e6a1b13b822b42e8fc89bdbb56b36cbce83d
ritz-p <okooko24816@gmail.com>
2024-12-18 22:46:36 +0900

    ABC 215 D

--- ABC/215/src/d.rs
+++ ABC/215/src/d.rs
@@ -1,5 +1,31 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
-    input! {};
+    input! {
+        n: usize,
+        m: usize,
+        mut a: [usize;n]
+    };
+    let mut flag = vec![true; 100_001];
+    flag[0] = false;
+    for i in a {
+        flag[i] = false;
+    }
+
+    let mut v = vec![true; m + 1];
+    let mut res = vec![1];
+    for i in 2..=m {
+        if !v[i] {
+            continue;
+        }
+        if (i..flag.len()).step_by(i).all(|j| flag[j]) {
+            res.push(i);
+        } else {
+            (i..=m).step_by(i).for_each(|j| v[j] = false);
+        }
+    }
+
+    println!("{}", res.len());
+    println!("{}", res.iter().join("\n"));
 }
