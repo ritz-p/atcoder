d0f2723a0d88577697127cd9d16301f6eff3eb50
ritz-p <okooko24816@gmail.com>
2024-12-31 18:08:43 +0900

    ABC 249 D

--- ABC/249/src/d.rs
+++ ABC/249/src/d.rs
@@ -1,5 +1,24 @@
 use proconio::input;
 
 fn main() {
-    input! {};
+    input! {
+        n: usize,
+        a: [usize;n]
+    };
+    let m = 200001;
+    let mut v = vec![0; m];
+    for e in &a {
+        v[*e] += 1;
+    }
+    let mut res = 0;
+
+    for i in 1..m {
+        let mut r = 1;
+        while i * r < m {
+            res += v[i] * v[r] * v[i * r];
+            r += 1;
+        }
+    }
+
+    println!("{}", res);
 }
