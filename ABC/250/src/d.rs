b36b6fd703a7e73d86418c1df15f8e3bf53a5a99
ritz-p <okooko24816@gmail.com>
2025-01-11 19:22:32 +0900

    ABC 250 D

--- ABC/250/src/d.rs
+++ ABC/250/src/d.rs
@@ -1,5 +1,31 @@
 use proconio::input;
 
 fn main() {
-    input! {};
+    input! {
+        n: usize,
+    }
+    let limit = 1_000_002;
+    let mut prime = vec![true; limit];
+    let mut i = 2;
+    let mut res = 0;
+    let mut ns = vec![0; limit];
+    while i * i * i <= n {
+        ns[i] = ns[i - 1];
+        if prime[i] {
+            let mut j = 2;
+            while i * j < limit {
+                prime[i * j] = false;
+                j += 1;
+            }
+            let x = n / (i * i * i);
+            if x > i {
+                res += ns[i];
+            } else {
+                res += ns[x];
+            }
+            ns[i] += 1;
+        }
+        i += 1;
+    }
+    println!("{}", res);
 }
