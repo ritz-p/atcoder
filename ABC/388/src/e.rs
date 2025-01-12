2e95e4fc8fd26118fb6571e0642201567717d172
ritz-p <okooko24816@gmail.com>
2025-01-12 17:40:49 +0900

    ABC 388 E

--- ABC/388/src/e.rs
+++ ABC/388/src/e.rs
@@ -1,42 +1,28 @@
 use proconio::input;
-use std::collections::BTreeMap;
 
 fn main() {
     input! {
         n: usize,
         a: [usize;n]
     };
-    let mut bmap = BTreeMap::new();
-    for e in &a {
-        *bmap.entry(e).or_insert(0) += 1;
-    }
-    let mut res = 0;
-    for e in &a {
-        let Some(count) = bmap.get_mut(&e) else {
-            continue;
-        };
-        if *count == 0 {
-            continue;
-        }
-        *count -= 1;
-        if *count == 0 {
-            bmap.remove(&e);
-        }
-        let remove_key = if let Some((k, v)) = bmap.range_mut(e * 2..).next() {
-            res += 1;
-            *v -= 1;
-            if *v == 0 {
-                Some(*k)
-            } else {
-                None
+    let mut left = 0;
+    let mut right = n / 2 + 1;
+
+    while left + 1 < right {
+        let mid = (left + right) / 2;
+        let mut flag = true;
+        for i in 0..mid {
+            if a[i] * 2 > a[n - mid + i] {
+                flag = false;
+                break;
             }
+        }
+        if flag {
+            left = mid;
         } else {
-            None
-        };
-        if let Some(k) = remove_key {
-            bmap.remove(&k);
+            right = mid;
         }
     }
 
-    println!("{}", res);
+    println!("{}", left);
 }
