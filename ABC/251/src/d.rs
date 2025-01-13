1835a15dfc22156f45efc0f91a3952e357f20626
ritz-p <okooko24816@gmail.com>
2025-01-13 23:53:20 +0900

    ABC 251 D

--- ABC/251/src/d.rs
+++ ABC/251/src/d.rs
@@ -1,5 +1,16 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
-    input! {};
+    input! {
+        _w:usize
+    };
+    let mut res = vec![];
+    for i in 0..3 {
+        for j in 1..100 {
+            res.push(j * 100usize.pow(i));
+        }
+    }
+    println!("{}", res.len());
+    println!("{}", res.iter().join(" "));
 }
