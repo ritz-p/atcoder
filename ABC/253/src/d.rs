9b0c2edf51241e4668693c148a55a7eabe709a26
ritz-p <okooko24816@gmail.com>
2025-01-17 02:38:27 +0900

    ABC 253 D

--- ABC/253/src/d.rs
+++ ABC/253/src/d.rs
@@ -1,5 +1,29 @@
 use proconio::input;
 
 fn main() {
-    input! {};
+    input! {
+        n: usize,
+        a: usize,
+        b: usize,
+    };
+    let n_sum = n * (n + 1) / 2;
+    let a_sum = n / a * (n / a + 1) / 2 * a;
+    let b_sum = n / b * (n / b + 1) / 2 * b;
+    let lcm = lcm(a, b);
+    let ab_sum = n / lcm * (n / lcm + 1) / 2 * lcm;
+
+    println!("{}", n_sum - a_sum - b_sum + ab_sum);
+}
+
+fn gcd(mut a: usize, mut b: usize) -> usize {
+    while b != 0 {
+        let temp = b;
+        b = a % b;
+        a = temp;
+    }
+    a
+}
+
+fn lcm(a: usize, b: usize) -> usize {
+    a * b / gcd(a, b)
 }
