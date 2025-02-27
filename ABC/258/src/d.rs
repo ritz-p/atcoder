768598bd07b56ce438fea09c435a972aa44d6d9d
ritz-p <okooko24816@gmail.com>
2025-02-28 02:34:23 +0900

    ABC 258 D

--- ABC/258/src/d.rs
+++ ABC/258/src/d.rs
@@ -1,5 +1,19 @@
 use proconio::input;
 
 fn main() {
-    input! {};
+    input! {
+        n: usize,
+        x: usize,
+        ab: [(usize,usize);n]
+    };
+    let mut res = usize::MAX;
+    let mut sum = 0;
+    for (index, (a, b)) in ab.iter().enumerate() {
+        if index + 1 > x {
+            break;
+        }
+        sum += a + b;
+        res = res.min(sum + b * (x - index - 1));
+    }
+    println!("{}", res);
 }
