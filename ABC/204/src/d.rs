b5c068628886b24b58c560c2a3e1fc332bee5ad3
ritz-p <okooko24816@gmail.com>
2025-03-04 01:22:56 +0900

    ABC 204 D

--- ABC/204/src/d.rs
+++ ABC/204/src/d.rs
@@ -1,5 +1,25 @@
 use proconio::input;
 
 fn main() {
-    input! {};
+    input! {
+        n: usize,
+        t: [usize;n]
+    };
+    let sum = t.iter().sum::<usize>();
+    let mut dp = vec![vec![usize::MAX; sum + 1]; n + 1];
+    dp[0][0] = 0;
+    for i in 0..n {
+        for j in 0..=sum {
+            if dp[i][j] == usize::MAX {
+                continue;
+            }
+            dp[i + 1][j + t[i]] = dp[i + 1][j + t[i]].min(dp[i][j]);
+            dp[i + 1][j] = dp[i + 1][j].min(dp[i][j] + t[i]);
+        }
+    }
+    let mut res = usize::MAX;
+    for i in 0..=sum {
+        res = res.min(i.max(dp[n][i]));
+    }
+    println!("{}", res);
 }
