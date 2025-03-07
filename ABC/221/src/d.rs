53677b15f1c1178371a58c2a4e1c8f1af70372b3
ritz-p <okooko24816@gmail.com>
2025-03-07 23:30:38 +0900

    ABC 221 D

--- ABC/221/src/d.rs
+++ ABC/221/src/d.rs
@@ -1,5 +1,27 @@
 use proconio::input;
 
 fn main() {
-    input! {};
+    input! {
+        n: usize,
+        ab: [(usize,usize);n]
+    };
+    let mut v = vec![];
+    for &(a, b) in &ab {
+        v.push((a, 1));
+        v.push((a + b, -1));
+    }
+    v.sort_unstable();
+
+    let mut res = vec![0; n + 1];
+    let mut count = 0;
+
+    for i in 0..v.len() - 1 {
+        count += v[i].1;
+        res[count as usize] += v[i + 1].0 - v[i].0;
+    }
+
+    for i in 1..=n {
+        print!("{} ", res[i]);
+    }
+    println!();
 }
