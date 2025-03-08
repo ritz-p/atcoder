fbe9167177d31f98f9f1d7441445b8816ede4c17
ritz-p <okooko24816@gmail.com>
2025-03-09 04:43:54 +0900

    ABC 120 C

--- ARC/120/src/a.rs
+++ ARC/120/src/a.rs
@@ -1,4 +1,27 @@
 use proconio::input;
 fn main() {
-    input! {};
+    input! {
+        n: usize,
+        a: [usize;n]
+    };
+    let mut sum = vec![0; n];
+    let mut cums = vec![0; n];
+    let mut max = vec![0; n];
+
+    for i in 0..n {
+        if i > 0 {
+            sum[i] = sum[i - 1];
+            max[i] = max[i - 1];
+            cums[i] = cums[i - 1];
+        }
+
+        sum[i] += a[i];
+        max[i] = max[i].max(a[i]);
+        cums[i] += sum[i];
+    }
+
+    for i in 0..n {
+        let res = max[i] * (i + 1) + cums[i];
+        println!("{}", res);
+    }
 }
