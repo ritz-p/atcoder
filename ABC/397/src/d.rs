b79b425da5f948f4083314886070fb0d3fc4f74b
ritz-p <okooko24816@gmail.com>
2025-03-17 19:08:53 +0900

    ABC 397 D

--- ABC/397/src/d.rs
+++ ABC/397/src/d.rs
@@ -1,19 +1,40 @@
-use std::collections::HashMap;
-
 use proconio::input;
 fn main() {
     input! {
-        n: u128
+        n: i128
     };
-    let mut set = HashMap::new();
-    for i in 1..=1000000 {
-        set.insert(i * i * i + n, i);
-    }
-    for x in 1..=1000000 {
-        if let Some(y) = set.get(&(x * x * x)) {
-            println!("{} {}", x, y);
+
+    for d in 1.. {
+        if d * d * d > n {
+            break;
+        }
+        if n % d != 0 {
+            continue;
+        }
+        let m = n / d;
+        let k = f(3, 3 * d, d * d - m);
+        if k > 0 {
+            println!("{} {}", k + d, k);
             return;
         }
     }
     println!("-1");
+}
+
+fn f(a: i128, b: i128, c: i128) -> i128 {
+    let mut low = 0;
+    let mut high = 600000001;
+
+    while high - low > 1 {
+        let mid = (high + low) / 2;
+        if a * mid * mid + b * mid + c <= 0 {
+            low = mid;
+        } else {
+            high = mid;
+        }
+    }
+    if a * low * low + b * low + c == 0 {
+        return low;
+    }
+    -1
 }
