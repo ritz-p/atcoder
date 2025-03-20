8f27b72d9c0a8a10f848c13922c8816b1f8f9069
ritz-p <okooko24816@gmail.com>
2025-03-21 01:46:49 +0900

    ABC 217 D

--- ABC/217/src/d.rs
+++ ABC/217/src/d.rs
@@ -1,5 +1,34 @@
+use std::collections::BTreeSet;
+
 use proconio::input;
 
 fn main() {
-    input! {};
+    input! {
+        l: usize,
+        q: usize,
+        cx: [(usize,usize);q]
+    };
+    let mut bset = BTreeSet::new();
+
+    for (c, x) in &cx {
+        match c {
+            1 => {
+                bset.insert(x);
+            }
+            2 => {
+                let high = bset.range(*x..).next();
+                let low = bset.range(..=*x).next_back();
+                if let (Some(high), Some(low)) = (high, low) {
+                    println!("{}", *high - *low);
+                } else if let Some(high) = high {
+                    println!("{}", high);
+                } else if let Some(low) = low {
+                    println!("{}", l - *low);
+                } else {
+                    println!("{}", l);
+                }
+            }
+            _ => {}
+        }
+    }
 }
