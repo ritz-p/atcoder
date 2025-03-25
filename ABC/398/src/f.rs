3cf4d7498b22967889e11de54f582d96433df1c7
ritz-p <okooko24816@gmail.com>
2025-03-25 19:50:58 +0900

    ABC 398 F

--- ABC/398/src/f.rs
+++ ABC/398/src/f.rs
@@ -3,26 +3,44 @@ fn main() {
     input! {
         s: Chars,
     };
-    let s_str: String = s.iter().collect();
-    let l = s_str.len();
-    let rev: String = s_str.chars().rev().collect();
-    let t = format!("{}#{}", s_str, rev);
-    let t_bytes = t.as_bytes();
-    let m = t.len();
-
-    let mut pi = vec![0; m];
-    for i in 1..m {
-        let mut j = pi[i - 1];
-        while j > 0 && t_bytes[i] != t_bytes[j] {
-            j = pi[j - 1];
-        }
-        if t_bytes[i] == t_bytes[j] {
-            j += 1;
-        }
-        pi[i] = j;
+    let mut cs = vec!['$'];
+    for c in &s {
+        cs.push(*c);
+        cs.push('$');
     }
 
-    let k = pi[m - 1];
-    let append: String = s_str[..(l - k)].chars().rev().collect();
-    println!("{}{}", s_str, append);
+    let res = manachar(cs);
+    let mut k = 0;
+    for i in s.len().. {
+        if res[i] >= s.len() - k {
+            break;
+        }
+        k += 1;
+    }
+
+    print!("{}", s.iter().collect::<String>());
+    for i in 0..k {
+        print!("{}", s[k - 1 - i]);
+    }
+    println!();
+}
+
+fn manachar(s: Vec<char>) -> Vec<usize> {
+    let mut i = 0;
+    let mut j = 0;
+    let mut res = vec![0; s.len()];
+    while i < s.len() {
+        while i >= j && i + j < s.len() && s[i - j] == s[i + j] {
+            j += 1;
+        }
+        res[i] = j;
+        let mut k = 1;
+        while i >= k && k + res[i - k] < j {
+            res[i + k] = res[i - k];
+            k += 1;
+        }
+        i += k;
+        j -= k;
+    }
+    res
 }
