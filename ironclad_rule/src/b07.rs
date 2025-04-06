e081ea4978063bc76b220b78e1e2e281ed3b9731
ritz-p <okooko24816@gmail.com>
2025-04-06 19:36:10 +0900

    ironclad rules B07

--- ironclad_rule/src/b07.rs
+++ ironclad_rule/src/b07.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
@@ -6,19 +7,16 @@ fn main() {
         n: usize,
         lr: [(usize,usize);n]
     };
-    let mut at = vec![0; t + 1];
+    let mut sums = vec![0; t + 1];
+    let mut plus = vec![0; t];
+    let mut minus = vec![0; t + 1];
     for (l, r) in lr {
-        at[l] += 1;
-        at[r] -= 1;
+        plus[l] += 1;
+        minus[r] += 1;
     }
-
-    let mut res = vec![0; t + 1];
-    res[0] = at[0];
-    for i in 1..t {
-        res[i] += res[i - 1] + at[i];
-    }
-
     for i in 0..t {
-        println!("{}", res[i]);
+        sums[i + 1] += plus[i] - minus[i] + sums[i];
     }
+
+    println!("{}", sums.iter().skip(1).join("\n"));
 }
