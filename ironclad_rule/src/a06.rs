7b48d6477a5980ddf45484a42ae2eb6556631520
ritz-p <okooko24816@gmail.com>
2025-04-06 18:06:01 +0900

    ironclad rules A06

--- ironclad_rule/src/a06.rs
+++ ironclad_rule/src/a06.rs
@@ -5,18 +5,13 @@ fn main() {
         n: usize,
         q: usize,
         a: [usize;n],
-    }
+        lr: [(usize,usize);q]
+    };
     let mut sums = vec![0; n + 1];
-    sums[1] = a[0];
-    for i in 2..=n {
-        sums[i] = a[i - 1] + sums[i - 1];
+    for i in 0..n {
+        sums[i + 1] = a[i] + sums[i];
     }
-    for _i in 0..q {
-        input! {
-            mut l: usize,
-            r: usize,
-        };
-        let res = sums[r] - sums[l - 1];
-        println!("{}", res);
+    for (l, r) in &lr {
+        println!("{}", sums[*r] - sums[l - 1]);
     }
 }
