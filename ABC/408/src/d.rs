2a9ee41aa6ed12e6540f690c8ad9e117da1abf06
ritz-p <okooko24816@gmail.com>
2025-06-05 00:55:30 +0900

    ABC 408 D

--- ABC/408/src/d.rs
+++ ABC/408/src/d.rs
@@ -1,4 +1,33 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 fn main() {
-    input! {};
+    input! {
+        t: usize,
+    };
+    let mut ns = vec![];
+    for _i in 0..t {
+        input! {
+            n: usize,
+            s: Chars
+        };
+        ns.push((n, s));
+    }
+
+    for (n, s) in ns {
+        let mut c = vec![0_isize; n + 1];
+        for i in 0..n {
+            if s[i] == '0' {
+                c[i + 1] = c[i] + 1;
+            } else {
+                c[i + 1] = c[i] - 1;
+            }
+        }
+        let sum = s.iter().filter(|e| **e == '1').count() as isize;
+        let mut res = 0;
+        let mut current = 0;
+        for i in 0..=n {
+            res = res.min(c[i] - current);
+            current = current.max(c[i]);
+        }
+        println!("{}", sum + res);
+    }
 }
