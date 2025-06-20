69468b93e6a35425e80dfaf1212da230ac2e9e16
ritz-p <okooko24816@gmail.com>
2025-06-21 03:19:12 +0900

    ARC 113 B

--- ARC/113/src/b.rs
+++ ARC/113/src/b.rs
@@ -1,27 +1,41 @@
-use itertools::Itertools;
+use std::collections::HashSet;
+
 use proconio::input;
+
 fn main() {
     input! {
-        n: usize,
-        a: [usize;n],
-        mut b: [usize;n]
+        a: usize,
+        b: usize,
+        c: usize,
     };
-    let mut res = vec![];
-    b.sort();
-    for i in 0..n {
-        let x = a[0] ^ b[i];
-        let mut c = vec![];
-        for j in 0..n {
-            c.push(x ^ a[j]);
-        }
-        c.sort();
-        if b == c {
-            res.push(x);
+    let mut aset = HashSet::new();
+
+    let mut av = vec![a % 10];
+    aset.insert(a % 10);
+    let mut current = 1;
+    loop {
+        let next = (a * av[current - 1]) % 10;
+        if !aset.contains(&next) {
+            aset.insert(next);
+            av.push(next);
+            current += 1;
+        } else {
+            break;
         }
     }
-
-    res.sort();
-    res.dedup();
-    println!("{}", res.len());
-    println!("{}", res.iter().join("\n"));
+    let l = av.len();
+    let mut m = 1;
+    let mut base = b % l;
+    let mut exp = c;
+    while exp > 0 {
+        if exp % 2 == 1 {
+            m = m * base % l;
+        }
+        base = base * base % l;
+        exp /= 2;
+    }
+    if m == 0 {
+        m = l;
+    }
+    println!("{}", av[m - 1]);
 }
