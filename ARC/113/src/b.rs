cceb68f562d80ec4ad2b36019be08f36abe017ae
ritz-p <okooko24816@gmail.com>
2025-06-21 02:37:20 +0900

    ABC 124 B

--- ARC/113/src/b.rs
+++ ARC/113/src/b.rs
@@ -1,6 +1,27 @@
+use itertools::Itertools;
 use proconio::input;
 fn main() {
     input! {
-        k: usize,
+        n: usize,
+        a: [usize;n],
+        mut b: [usize;n]
     };
+    let mut res = vec![];
+    b.sort();
+    for i in 0..n {
+        let x = a[0] ^ b[i];
+        let mut c = vec![];
+        for j in 0..n {
+            c.push(x ^ a[j]);
+        }
+        c.sort();
+        if b == c {
+            res.push(x);
+        }
+    }
+
+    res.sort();
+    res.dedup();
+    println!("{}", res.len());
+    println!("{}", res.iter().join("\n"));
 }
