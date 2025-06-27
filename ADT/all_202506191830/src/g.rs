4f81fa3de78777ef9e81cb3448a10a18ec1825d7
ritz-p <okooko24816@gmail.com>
2025-06-28 01:49:15 +0900

    ADT 202506191830 G

--- ADT/all_202506191830/src/g.rs
+++ ADT/all_202506191830/src/g.rs
@@ -1,4 +1,42 @@
+use std::{cmp::Reverse, collections::BinaryHeap};
+
 use proconio::input;
 fn main() {
-    input! {};
+    input! {
+        n: usize,
+        mut td: [(usize,usize);n]
+    };
+
+    td.sort();
+    td.reverse();
+
+    let mut heap = BinaryHeap::new();
+    let mut time = 0;
+    let mut res = 0;
+    while !heap.is_empty() || !td.is_empty() {
+        if heap.is_empty() {
+            if let Some(&(t, _d)) = td.last() {
+                time = t;
+            }
+        }
+
+        while let Some(&(t, d)) = td.last() {
+            if t <= time {
+                heap.push(Reverse(t + d));
+                td.pop();
+            } else {
+                break;
+            }
+        }
+
+        while let Some(Reverse(x)) = heap.pop() {
+            if time <= x {
+                res += 1;
+                break;
+            }
+        }
+        time += 1;
+    }
+
+    println!("{}", res);
 }
