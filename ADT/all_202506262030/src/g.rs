46e038d9e5738b4ac2e594d1eede578e66c7a4a0
ritz-p <okooko24816@gmail.com>
2025-06-28 18:58:12 +0900

    ADt 202506262030 G

--- ADT/all_202506262030/src/g.rs
+++ ADT/all_202506262030/src/g.rs
@@ -1,33 +1,33 @@
 use proconio::input;
+
 fn main() {
     input! {
         n: usize,
         m: usize,
         mut l: [usize;n]
     };
-
-    l = l.iter().map(|e| e + 1).collect::<Vec<usize>>();
+    l = l.iter().map(|e| e + 1).collect();
     let mut left = l.iter().max().unwrap() - 1;
     let mut right = l.iter().sum::<usize>();
 
     while left + 1 < right {
-        let middle = (left + right) / 2;
+        let mid = (left + right) / 2;
 
-        let mut current = 1;
+        let mut wid = 1;
         let mut len = 0;
 
         for i in 0..n {
             len += l[i];
-            if len > middle {
-                current += 1;
+            if len > mid {
+                wid += 1;
                 len = l[i];
             }
         }
 
-        if current > m {
-            left = middle;
+        if wid > m {
+            left = mid;
         } else {
-            right = middle;
+            right = mid;
         }
     }
 
