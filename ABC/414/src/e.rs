5e4edfbf983a7ae4ae78a61b5e591f4fd2ee7e44
ritz-p <okooko24816@gmail.com>
2025-07-13 17:54:34 +0900

    ABC 414 E

--- ABC/414/src/e.rs
+++ ABC/414/src/e.rs
@@ -1,4 +1,18 @@
 use proconio::input;
 fn main() {
-    input! {};
+    input! {
+        n: usize,
+    };
+
+    let mut b = 1;
+    let m = 998244353;
+    let mut res = (n % m) * ((n + 1) % m) / 2;
+
+    while b * b <= n {
+        let current = n / b;
+        res -= ((((current % m) - b % m) * 2) % m + 1) % m;
+        b += 1;
+    }
+
+    println!("{}", res % m);
 }
