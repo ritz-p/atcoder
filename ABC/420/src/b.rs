5d939207d6f47656abf6db8d267cf02cbe1e3629
ritz-p <okooko24816@gmail.com>
2025-08-27 01:53:15 +0900

    ABC 420 B~C

--- ABC/420/src/b.rs
+++ ABC/420/src/b.rs
@@ -1,4 +1,47 @@
-use proconio::input;
+use std::collections::HashSet;
+
+use itertools::Itertools;
+use proconio::{input, marker::Chars};
 fn main() {
-    input! {};
+    input! {
+        n: usize,
+        m: usize,
+        ss: [Chars;n]
+    };
+    let mut zv = vec![HashSet::new(); m];
+    let mut ov = vec![HashSet::new(); m];
+
+    for (si, s) in ss.iter().enumerate() {
+        for (ci, c) in s.iter().enumerate() {
+            if *c == '0' {
+                zv[ci].insert(si);
+            } else {
+                ov[ci].insert(si);
+            }
+        }
+    }
+    let mut v = vec![0; n];
+    for i in 0..m {
+        if zv[i].len() < ov[i].len() && zv[i].len() > 0 {
+            for c in &zv[i] {
+                v[*c] += 1;
+            }
+        } else if ov[i].len() < zv[i].len() && ov[i].len() > 0 {
+            for c in &ov[i] {
+                v[*c] += 1;
+            }
+        }
+    }
+    let mut max = 0;
+    for i in 0..n {
+        max = max.max(v[i]);
+    }
+    let mut res = vec![];
+    for i in 0..n {
+        if v[i] == max {
+            res.push(i + 1);
+        }
+    }
+    res.sort();
+    println!("{}", res.iter().join(" "));
 }
--- ABC/420/src/c.rs
+++ ABC/420/src/c.rs
@@ -1,4 +1,43 @@
 use proconio::input;
 fn main() {
-    input! {};
+    input! {
+        n: usize,
+        q: usize,
+        a: [usize;n],
+        b: [usize;n],
+        cxv: [(char,usize,usize);q]
+    };
+    let mut sum = 0;
+    let mut mins = vec![];
+    for (ae, be) in a.iter().zip(b.iter()) {
+        sum += ae.min(be);
+        mins.push(*ae.min(be));
+    }
+
+    let mut ac = a.clone();
+    let mut bc = b.clone();
+
+    for (c, x, v) in cxv {
+        match c {
+            'A' => {
+                let current = mins[x - 1];
+                ac[x - 1] = v;
+                let min = ac[x - 1].min(bc[x - 1]);
+                mins[x - 1] = min;
+                sum += min;
+                sum -= current;
+                println!("{}", sum);
+            }
+            'B' => {
+                let current = mins[x - 1];
+                bc[x - 1] = v;
+                let min = ac[x - 1].min(bc[x - 1]);
+                mins[x - 1] = min;
+                sum += min;
+                sum -= current;
+                println!("{}", sum);
+            }
+            _ => {}
+        }
+    }
 }
