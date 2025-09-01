8c4869d7fbefe73a352adb83368cc661a3583e57
ritz-p <okooko24816@gmail.com>
2025-09-01 22:13:30 +0900

    ABC 418 D

--- ABC/418/src/d.rs
+++ ABC/418/src/d.rs
@@ -4,4 +4,25 @@ fn main() {
         n: usize,
         t: Chars
     };
+    let mut dp = vec![vec![0; 2]; n + 1];
+
+    for i in 1..=n {
+        match t[i - 1] {
+            '0' => {
+                dp[i][0] = dp[i - 1][1];
+                dp[i][1] = dp[i - 1][0] + 1;
+            }
+            '1' => {
+                dp[i][0] = dp[i - 1][0] + 1;
+                dp[i][1] = dp[i - 1][1];
+            }
+            _ => {}
+        }
+    }
+    let mut res: usize = 0;
+
+    for i in 1..=n {
+        res += dp[i][0];
+    }
+    println!("{}", res);
 }
