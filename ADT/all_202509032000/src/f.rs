ab0db4202cb5fe544b5de32762750e98f7daf95f
ritz-p <okooko24816@gmail.com>
2025-09-05 01:54:11 +0900

    ADT 202509032000 F

--- ADT/all_202509032000/src/f.rs
+++ ADT/all_202509032000/src/f.rs
@@ -1,4 +1,27 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 fn main() {
-    input! {};
+    input! {
+        n: usize,
+        m: usize,
+        s: [Chars;n]
+    };
+    let mut res = usize::MAX;
+    for b in 0..(1 << n) {
+        let mut f = vec![false; m];
+        let mut current = 0;
+        for i in 0..n {
+            if (b >> i) & 1 != 0 {
+                current += 1;
+                for j in 0..m {
+                    if s[i][j] == 'o' {
+                        f[j] = true;
+                    }
+                }
+            }
+        }
+        if f.iter().all(|flag| *flag) {
+            res = res.min(current);
+        }
+    }
+    println!("{}", res);
 }
