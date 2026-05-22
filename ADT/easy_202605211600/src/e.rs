2a428a67fd549a32d2adb440007fef16b1a3df48
ritz-p <okooko24816@gmail.com>
2026-05-22 15:25:26 +0900

    ADT 202605211600 easy e

--- ADT/easy_202605211600/src/e.rs
+++ ADT/easy_202605211600/src/e.rs
@@ -1,4 +1,34 @@
+use itertools::Itertools;
 use proconio::input;
 fn main() {
-    input! {};
+    input! {
+        n: usize,
+        lr: [(isize,isize);n]
+    };
+    let mut res: Vec<isize> = vec![0; n];
+    let mut sum = 0;
+    let mut x = 0;
+
+    for (i, &(l, r)) in lr.iter().enumerate() {
+        res[i] = l;
+        sum += l;
+        x += r;
+    }
+
+    if sum > 0 || x < 0 {
+        println!("No");
+        return;
+    }
+
+    for (i, &(l, r)) in lr.iter().enumerate() {
+        if sum + (r - l) > 0 {
+            res[i] = l + sum.abs_diff(0) as isize;
+            break;
+        } else {
+            res[i] = r;
+            sum += r - l;
+        }
+    }
+    println!("Yes");
+    println!("{}", res.iter().join(" "));
 }
